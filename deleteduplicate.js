fce78328d4a2de319ff03457fabd0cce9c1c7297
aklavya20 <aklavya36@gmail.com>
2025-06-30 16:52:39 +0530

    initial commit

--- deleteduplicate.js
+++ deleteduplicate.js
@@ -1,29 +1,15 @@
-// let n;
-// let arr = [1, 3, 7, 2, 1, 3, 7];
-// const deleteDuplicate = () => {
-//   for (let i = 0; i < arr.length; i++) {
-//     for (let j = i + 1; j < arr.length; j++) {
-//       if (arr[i] == arr[j]) {
-//         arr.splice(j, 1);
-//         j--;
-//       }
-//     }
-//   }
-//   console.log(arr);
-// };
-
-// deleteDuplicate();
-
+let n;
 let arr = [1, 3, 7, 2, 1, 3, 7];
-
-let myArr = [];
-let count = {};
-
-for (let i = 0; i < arr.length; i++) {
-  let item = arr[i];
-  if (!(item in count)) {
-    count[item] = true;
-    myArr.push(item);
+const deleteDuplicate = () => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] == arr[j]) {
+        arr.splice(j, 1);
+        j--;
+      }
+    }
   }
-}
-console.log(myArr);
+  console.log(arr);
+};
+
+deleteDuplicate();
---
+++ deleteduplicateinarray.js
@@ -0,0 +1,13 @@
+let newarr = [1, 3, 7, 2, 1, 3, 7];
+
+let myArr = [];
+let count = {};
+
+for (let i = 0; i < newarr.length; i++) {
+  let item = newarr[i];
+  if (!(item in count)) {
+    count[item] = true;
+    myArr.push(item);
+  }
+}
+console.log(myArr);
