d3c8e6354b4cc8d27ac81cdd840d4e5107c7924e
aklavya20 <aklavya36@gmail.com>
2025-06-30 16:47:54 +0530

    initial commit

---
+++ deleteduplicate.js
@@ -0,0 +1,29 @@
+// let n;
+// let arr = [1, 3, 7, 2, 1, 3, 7];
+// const deleteDuplicate = () => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] == arr[j]) {
+//         arr.splice(j, 1);
+//         j--;
+//       }
+//     }
+//   }
+//   console.log(arr);
+// };
+
+// deleteDuplicate();
+
+let arr = [1, 3, 7, 2, 1, 3, 7];
+
+let myArr = [];
+let count = {};
+
+for (let i = 0; i < arr.length; i++) {
+  let item = arr[i];
+  if (!(item in count)) {
+    count[item] = true;
+    myArr.push(item);
+  }
+}
+console.log(myArr);
