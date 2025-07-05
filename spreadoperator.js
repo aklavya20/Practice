c9918f18ff051657facc5074a81cc788bfd2da44
aklavya20 <aklavya36@gmail.com>
2025-07-05 11:25:25 +0530

    initial commit

--- spreadoperator.js
+++ spreadoperator.js
@@ -1,4 +1,22 @@
-let arr = [1,2,3];
-let newArr = [...arr,4,5]
+let arr = [1, 2, 3];
+let newArr = [...arr, 4, 5];
 
 console.log(newArr);
+
+const car = {
+  brand: "Ford",
+  model: "Mustang",
+  color: "red",
+  power: "800HP",
+};
+
+const newCar = {
+  type: "Muscle Car",
+  year: "2024",
+  color: "yellow",
+  power: "1200HP",
+};
+
+const Car2024 = { ...car, ...newCar };
+
+console.log(Car2024);
